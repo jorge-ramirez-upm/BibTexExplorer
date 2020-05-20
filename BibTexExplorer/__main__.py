781ab27d32040236e170b4dc53ca3a6c851ce7a3
Jorge Ramirez <jorge.ramirez@upm.es>
2020-05-20 12:09:10 +0200

    Added Application Title and Icon

--- BibTexExplorer/__main__.py
+++ BibTexExplorer/__main__.py
@@ -1,6 +1,8 @@
 import sys
+import os
 
 from PyQt5.QtWidgets import QApplication, QMessageBox
+from PyQt5.QtGui import QIcon
 from BibTexExplorer.bibfiletools import parse_bibfile
 from BibTexExplorer.TableWidget import TableWidget
 
@@ -16,7 +18,10 @@ def main():
             )
         sys.exit(1)
 
+    scriptDir = os.path.dirname(os.path.realpath(__file__))
+    app.setWindowIcon(QIcon(scriptDir + os.path.sep + 'icons'+ os.path.sep+ 'Everaldo-Desktoon-Library.ico'))
     w = TableWidget()
+    w.setWindowTitle("BibTex Explorer")
     
     w.populate(d)
     w.show()
